8ad7bc8d41933898bd7e35a3633efc3652c33c39
Johannes Millan <johannes.millan@gmail.com>
2015-03-14 20:55:03 +0100

    initial commit

---
+++ src/angular-example-p.js
@@ -0,0 +1,44 @@
+angular.module('angularExample')
+    .provider('angularExample', function angularExampleProvider()
+    {
+        'use strict';
+
+        // *****************
+        // DEFAULTS & CONFIG
+        // *****************
+
+        var config = {
+            spinnerTpl: '<span class="btn-spinner"></span>',
+            disableBtn: true,
+            btnLoadingClass: 'is-loading'
+        };
+
+
+        // *****************
+        // SERVICE-FUNCTIONS
+        // *****************
+
+
+        // *************************
+        // PROVIDER-CONFIG-FUNCTIONS
+        // *************************
+
+        return {
+            extendConfig: function (newConfig)
+            {
+                config = angular.extend(config, newConfig);
+            },
+
+
+            // ************************************************
+            // ACTUAL FACTORY FUNCTION - used by the directive
+            // ************************************************
+
+            $get: function ()
+            {
+                return {
+                    config: config
+                };
+            }
+        };
+    });
